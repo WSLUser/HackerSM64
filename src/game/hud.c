4e337996bccd4290db347ecd6637565cf4ddae84
Reonu <danileon95@gmail.com>
2022-06-29 17:02:01 +0100

    Combined ENABLE_LIVES and DEFAULT_NUM_LIVES, and removed SAVE_NUM_LIVES

--- src/game/hud.c
+++ src/game/hud.c
@@ -32,7 +32,7 @@
 #define HUD_POWER_METER_HIDDEN_Y     300
 
 #ifdef BREATH_METER
-// #ifdef DISABLE_LIVES
+// #ifndef ENABLE_LIVES
 // #define HUD_BREATH_METER_X         64
 // #define HUD_BREATH_METER_Y        200
 // #define HUD_BREATH_METER_HIDDEN_Y 300
@@ -563,7 +563,7 @@ void render_hud(void) {
             render_hud_cannon_reticle();
         }
 
-#ifndef DISABLE_LIVES
+#ifdef ENABLE_LIVES
         if (hudDisplayFlags & HUD_DISPLAY_FLAG_LIVES) {
             render_hud_mario_lives();
         }
